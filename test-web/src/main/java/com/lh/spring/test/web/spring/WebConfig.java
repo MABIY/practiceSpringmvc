4cf0cb1071d4d8a544a2efaa0f38b7d5de08fbcb
lh <jyfc7879@gmail.com>
2018-03-19 22:48:56 +0800

    add multil viewResolver

--- test-web/src/main/java/com/lh/spring/test/web/spring/WebConfig.java
+++ test-web/src/main/java/com/lh/spring/test/web/spring/WebConfig.java
@@ -3,25 +3,56 @@ package com.lh.spring.test.web.spring;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackageClasses = {com.lh.spring.test.application.Components.class, com.lh.spring.test.web.controller.Components.class})
-public class WebConfig {
-    //@Bean
-    //public ViewResolver internalResourceViewResolver() {
-    //    InternalResourceViewResolver bean = new InternalResourceViewResolver();
-    //    bean.setViewClass(JstlView.class);
-    //    bean.setPrefix("/jsp/");
-    //    bean.setSuffix(".jsp");
-    //    return bean;
-    //}
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.ignoreAcceptHeader(true).defaultContentType(
+                MediaType.TEXT_HTML);
+    }
+
+
+    /*
+     * Configure ContentNegotiatingViewResolver
+     */
+    @Bean
+    public ViewResolver contentNegotiatingViewResolver(ContentNegotiationManager manager) {
+        ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+        resolver.setContentNegotiationManager(manager);
+
+        // Define all possible view resolvers
+        List<ViewResolver> resolvers = new ArrayList<ViewResolver>();
+        // chain 查找 不好识别固定的解析器
+        resolvers.add(freemarkerViewResolver());
+        resolvers.add(jspViewResolver());
+        resolver.setViewResolvers(resolvers);
+        return resolver;
+    }
+    @Bean
+    public ViewResolver jspViewResolver() {
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setViewClass(JstlView.class);
+        bean.setPrefix("/jsp/");
+        bean.setSuffix(".jsp");
+        return bean;
+    }
 
     @Bean
     public FreeMarkerViewResolver freemarkerViewResolver() {
