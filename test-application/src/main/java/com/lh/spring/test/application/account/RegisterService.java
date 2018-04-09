c494a4e16bd9da3db1a512754345e96f1f6f6d38
liuhua <liuhua@vipmro.net>
2018-04-09 13:03:50 +0800

    try memcached config

--- test-application/src/main/java/com/lh/spring/test/application/account/RegisterService.java
+++ test-application/src/main/java/com/lh/spring/test/application/account/RegisterService.java
@@ -1,18 +1,29 @@
 package com.lh.spring.test.application.account;
 
+import com.google.code.ssm.api.ParameterValueKeyProvider;
+import com.google.code.ssm.api.ReadThroughSingleCache;
 import com.lh.spring.test.domain.account.AccountService;
+import net.rubyeye.xmemcached.MemcachedClient;
+import net.rubyeye.xmemcached.exception.MemcachedException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeoutException;
 
 @RestController
 public class RegisterService {
     @Autowired
     AccountService accountService;
 
-    @RequestMapping(value = "/register", method = RequestMethod.GET)
-    public void register() {
+    @RequestMapping(value = "/register/{id}", method = RequestMethod.GET)
+    @Cacheable(value = "addresses")
+    public String register(@PathVariable(value = "id")String id) throws InterruptedException, MemcachedException, TimeoutException {
         System.out.println("fdsafdas");
+        return "tesafg";
     }
 }
---
+++ test-infrastructure/src/main/java/com/lh/spring/test/infrastructure/spring/MemcachedCachingConfig.java
@@ -0,0 +1,11 @@
+package com.lh.spring.test.infrastructure.spring;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by LiuHua on 18-4-9.
+ */
+@Configuration
+//@EnableCaching
+public class MemcachedCachingConfig {
+}
